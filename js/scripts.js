62cdb76401543ea75c1f6b9a9afe5be83de64ba0
HarryStevens <harryjosephstevens@gmail.com>
2015-03-04 02:37:37 -0500

    about scroll

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,19 @@
 var WindowY = $(window).height();
 var WindowX = $(window).width();
 
+function goToByScroll(id) {
+	$('html,body').animate({
+		scrollTop : $(id).offset().top
+	}, 'slow');
+}
+
+
 $(document).ready(function() {
 	console.log(WindowX);
 	$('.banner .img').css('width', WindowX);
+
+	$('.navbar a').click(function() {
+		goToByScroll($(this).attr('href'));
+		return false;
+	});
 });
